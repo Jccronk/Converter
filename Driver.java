3da2f696e35203be2dd5d0f4d4a2b2e4cb7bf44b
cronk053 <cronk053@umn.edu>
2023-01-09 17:18:43 -0600

    Last version with unit as int?

--- Driver.java
+++ Driver.java
@@ -11,7 +11,7 @@ public class Driver {
         System.out.println("Enter an integer and a string separated by a comma: ");
         System.out.println("Units available are feet, inches, kilograms, pounds, celcius, fahrenheit ");
 
-        int unit = -1;
+        int unit = Integer.MIN_VALUE;
         String type = "";
 
         try {
@@ -20,15 +20,11 @@ public class Driver {
             unit = Integer.parseInt(parts[0].trim());
             type = parts[1].trim();
             scanner.close();
+            convertAndPrint(unit, type);
         } catch (NumberFormatException e) {
             System.out.println("Invalid input! Please enter an integer followed by a string.");
         }
 
-        if (unit == -1) {
-            throw new IllegalArgumentException("Invalid value for 'unit': " + unit);
-        }
-
-        convertAndPrint(unit, type);
     }
 
     private static void convertAndPrint(int unit, String type) {
@@ -37,19 +33,20 @@ public class Driver {
                 System.out.println(unit + " feet = " + unit * 12 + " inches");
                 break;
             case "inches":
-                System.out.println(unit + " inches = " + (double) unit / 12 + " feet");
+                System.out.println(unit + " inches = " + String.format("%.2f", (double) unit / 12) + " feet");
                 break;
             case "kilograms":
-                System.out.println(unit + " kilograms = " + unit * 2.20462 + " pounds");
+                System.out.println(unit + " kilograms = " + String.format("%.2f", unit * 2.20462) + " pounds");
                 break;
             case "pounds":
-                System.out.println(unit + " pounds = " + unit / 2.20462 + " kilograms");
+                System.out.println(unit + " pounds = " + String.format("%.2f", unit / 2.20462) + " kilograms");
                 break;
             case "celsius":
-                System.out.println(unit + " Celsius = " + (unit * 9 / 5) + 32 + " Fahrenheit");
+                System.out.println(unit + " Celsius = " + String.format("%.2f", (unit * 9 / 5) + 32) + " Fahrenheit");
                 break;
             case "fahrenheit":
-                System.out.println(unit + " degrees Fahrenheit = " + (unit - 32) * (5.0 / 9.0) + " degrees Celsius");
+                System.out.println(unit + " degrees Fahrenheit = " + String.format("%.2f", (unit - 32) * (5.0 / 9.0))
+                        + " degrees Celsius");
                 break;
             default:
                 System.out.println("Something went wrong");
